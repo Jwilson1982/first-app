52d094a10140246c24fcf48a419e2154d793d6da
Janell <janell2@Janells-MacBook-Pro.local>
2013-09-11 15:24:09 -0700

    Initial Commit

---
+++ It's a girl/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  It's a girl
+//
+//  Created by Janell on 9/11/13.
+//  Copyright (c) 2013 Janell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
